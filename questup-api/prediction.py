f176c25789b3dcd21fcbdd8a018115d38b2c33d7
Louis Born <louis.born@check24.de>
2023-01-27 11:06:12 +0100

    added documentation

--- questup-api/prediction.py
+++ questup-api/prediction.py
@@ -1,43 +1,47 @@
 import numpy as np
 import pandas as pd
-# from sklearn import *
+import math
+from joblib import dump, load
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-# from sklearn.metrics import *
-from joblib import dump, load
+
+DEFAULT_PD_DATA = [2, 2, 3, 10, 10, 1, 1, 2]  # The input if no other input data is given.
+FEATURES = ['studytime', 'failures', 'absences', 'G1', 'G2', 'freetime', 'goout',
+            'traveltime']  # The features which are used to predict the grade.
 
 
 class GradePredictionController:
     """
-
+    A class to train and predict a students grade based on eight constant features.
     """
-    def __init__(self, prediction_data=None):
-        if prediction_data is None:
-            prediction_data = [2, 0, 3, 10, 10]
-        self.storage = 'grade_prediction_model.joblib'
-        self.data = pd.read_csv('data/student-mat.csv')
-        self.features = ['studytime', 'failures', 'absences', 'G1', 'G2']
-        self.prediction_data = prediction_data
+
+    def __init__(self, input_arr=None):
+        if input_arr is None:
+            input_arr = DEFAULT_PD_DATA
+        self.model_storage_link = 'grade_prediction_model.joblib'
+        self.training_data = pd.read_csv('data/student-mat.csv')
+        self.features = FEATURES
+        self.input_arr = input_arr
 
     def train_model(self):
-        trainer = Trainer(self.storage, student_data=self.data, selected_features=self.features)
+        trainer = Trainer(parent=self)
         trainer.persist_model()
-        return 'Okay'
 
     def predict(self):
-        predicter = Predicter(self.storage, self.prediction_data)
+        predicter = Predicter(parent=self)
         return predicter.predict()
 
 
 class Trainer:
-    def __init__(self, storage, student_data, selected_features):
-        self.storage = storage
-        self.student_data = student_data
-        self.selected_features = selected_features
+    def __init__(self, parent):
+        self.parent = parent  # The instance to the controller.
         self.model = self._train_model()
 
     def persist_model(self):
-        dump(self.model, self.storage)
+        """
+        Stores the trained model at the defined model storage link.
+        """
+        dump(self.model, self.parent.model_storage_link)
 
     def _train_model(self):
         X, Y = self._get_x_y()
@@ -47,38 +51,37 @@ class Trainer:
         return model
 
     def _get_x_y(self):
-        X = (self.student_data[self.selected_features]).values
-        Y = self.student_data['G3']
+        X = (self.parent.training_data[self.parent.features]).values
+        Y = self.parent.training_data['G3']
         return X, Y
 
     def _train(self, X, Y):
+        """
+        Parameters
+        ----------
+        :param X: The features data.
+        :param Y: The data to be predicted by the model.
+        """
         X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.4, random_state=20, shuffle=True)
         return X_train, X_test, Y_train, Y_test
 
 
 class Predicter:
-    def __init__(self, storage, prediction_data):
-        self.storage = storage
-        self.prediction_data = prediction_data
-        self.model = self.load_model()
+    def __init__(self, parent):
+        self.parent = parent # The instance to the controller.
+        self.model = None  # The pre-trained model
+        self.load_model()
 
     def load_model(self):
-        return load(self.storage)
+        """
+        Loads the pre-trained model from the storage. If the model does not exist
+        it trains and stores a new model.
+        """
+        try:
+            self.model = load(self.parent.model_storage_link)
+        except FileNotFoundError as e:
+            self.parent.train_model()
+            self.load_model()
 
     def predict(self):
-        return self.model.predict(np.array(self.prediction_data).reshape((1, -1)))[0]
-
-# student_data['G4'] = model.predict(np.array(input_to_guess).reshape((1, -1)))[0]
-# print(model.predict(np.array(input_to_guess).reshape((1, -1)))[0])
-
-# it would also be possible to make a prediction for a particular row in the dataset
-# print(model.predict((student_data.iloc[44-2][best_features]).values.reshape(1,-1)), "guessed_
-
-
-# c = GradePredictionController()
-# print(c.train_model())
-# print(c.predict())
-#
-# # prediction data in predict übergeben
-# c.prediction_data = [2, 0, 3, 10, 20]
-# print(c.predict())
+        return math.ceil(self.model.predict(np.array(self.parent.input_arr).reshape((1, -1)))[0])
